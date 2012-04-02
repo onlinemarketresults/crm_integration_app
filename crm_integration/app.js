693e7dcba3ce01da048afe4a023bb98d787af31a
James A. Rosen <james@zendesk.com>
2012-04-02 15:54:03 -0700

    Adding ZendeskApps.defineApp
    App.install needs the location for the installation, but no app has been
    created. Thus, we need the location information at the class level, not
    the instance level. ZendeskApps.defineApp(location, properties) sets the
    location on the new App subclass.

--- crm_integration/app.js
+++ crm_integration/app.js
@@ -1,7 +1,6 @@
 (function() {
 
-  return ZendeskApps.App.extend({
-    location: ZendeskApps.Site.TICKET_PROPERTIES,
+  return ZendeskApps.defineApp(ZendeskApps.Site.TICKET_PROPERTIES, {
     appID: '/apps/01-crm-integration/versions/1.0.0',
     name: 'CRM Integration',
 
